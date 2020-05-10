4727224739ea6b72df7e9afea5c105ed332adb0d
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-05-09 21:32:34 -0400

    src(ae): add plot_history function

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -11,6 +11,10 @@ FUNCTIONS
 
     build_dense_ae_architecture()
         Builds and compiles a tensorflow.keras dense autoencoder network
+
+    plot_history()
+        Plot training and validation loss using keras history object
+
 """
 
 # import and set seeds for reproducible results
@@ -27,6 +31,9 @@ from tensorflow.keras import Input
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import Adam
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def build_dense_ae_architecture(input_dim, encoding_dim, droprate,
@@ -99,3 +106,55 @@ def build_dense_ae_architecture(input_dim, encoding_dim, droprate,
     )
 
     return ae, encoder, decoder
+
+
+def plot_history(history, title, val_name='validation', loss_type='MSE'):
+    """Plot training and validation loss using keras history object
+    
+    :param history: keras training history object or dict. If a dict is
+                    used, it must have two keys named 'loss' and 'val_loss'
+                    for which the corresponding values must be lists or
+                    arrays with float values
+    :param title: string, the title of the resulting plot
+    :param val_name: string, the name for the val_loss line in the plot
+                     legend (default 'validation')
+    :param loss_type: string, the loss type name to be printed as the
+                      y axis label (default 'MSE')
+    
+    :return: a line plot illustrating model training history, no
+             objects are returned
+    """
+    if type(history)==dict:
+        n_epochs = len(history['loss'])
+        loss = history['loss']
+        val_loss = history['val_loss']
+    else:
+        n_epochs = len(history.history['loss'])
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+
+    x_vals = np.arange(1, n_epochs+1)
+    
+    # adjust interval of x_ticks based on n_epochs
+    if n_epochs<40:
+        x_ticks = x_vals
+    elif n_epochs<140:
+        x_ticks = np.arange(0, n_epochs+1, 5)
+    else:
+        x_ticks = np.arange(0, n_epochs+1, 10)
+    
+    fig, ax = plt.subplots(1, 1, figsize=(12, 5))
+
+    plt.suptitle('{}'.format(title), fontsize=18)
+        
+    ax.plot(x_vals, loss, 'k--', label='training')
+    ax.plot(x_vals, val_loss, 'k-', label=val_name)
+    ax.set_xlabel('epoch', fontsize=14)
+    ax.set_ylabel('loss ({})'.format(loss_type), fontsize=14)
+    ax.set_xticks(x_ticks)
+    ax.grid(':', alpha=0.4)
+    ax.tick_params(labelsize=12)        
+    
+    plt.legend(fontsize=14)
+    plt.tight_layout(rect=[0, 0.03, 1, .9])
+    plt.show()
