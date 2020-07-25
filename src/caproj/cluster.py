d144cbef77c5235e5e06bd530ce6a84477fb7d6b
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-07-25 08:07:26 -0400

    src(cluster): lint module and improve docstrings

--- src/caproj/cluster.py
+++ src/caproj/cluster.py
@@ -1,77 +1,55 @@
 """
+caproj.cluster
+~~~~~~~~~~~~~~
+
 This module contains functions for visualizing data and model results
 
-FUNCTIONS
+**Module classes:**
 
-    silplot()
-        Generates silhouette subplot of kmeans clusters alongside PCA n=2
+.. autosummary::
 
-    display_gapstat_with_errbars()
-        Generates plots of gap stats with error bars for each number of
-        clusters
+   UMAP_embedder
 
-    fit_neighbors()
-        Fits n nearest neighbors based on min samples and returns distances
+**Module functions:**
 
-    plot_epsilon()
-        Plot epsilon by index sorted by increasing distance
+.. autosummary::
 
-    silscore_dbscan()
-        Generates sil score ommitting observations not assigned to any cluster
-        by dbscan
-
-    fit_dbscan()
-        Fits dbscan and returns dictionary of results including model, labels,
-        indices
-    
-    print_dbscan_results()
-        Prints summary results of fitted dbscan_dict
-
-    plot_dendrogram()
-        Plots a dendrogram given a set of input hierarchy linkage data
-
-    plot_cluster_hist()
-        Requires melted dataframe as input and plots histograms by cluster
-
-    plot_umap_scatter()
-        plots scatterplot with color scale
-
-    plot_category_scatter()
-        plots scatterplot with categories colors
-
-CLASSES
-
-    UMAP_embedder()
-        Used for UMAP embedding section of final report
+   silplot
+   display_gapstat_with_errbars
+   fit_neighbors
+   plot_epsilon
+   silscore_dbscan
+   fit_dbscan
+   print_dbscan_results
+   plot_dendrogram
+   plot_cluster_hist
+   plot_umap_scatter
+   plot_category_scatter
 
 """
 
 from math import pi
 
-import pandas as pd
-import numpy as np
+import hdbscan
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
-
-from sklearn.decomposition import PCA
+import numpy as np
+import pandas as pd
 import scipy.cluster.hierarchy as hac
-from scipy.spatial.distance import pdist
-from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from sklearn.cluster import DBSCAN
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import silhouette_samples, silhouette_score
-import matplotlib.cm as cm
 
-from sklearn.cluster import KMeans
-from gap_statistic import OptimalK
-import scipy.cluster.hierarchy as hac
-
-from .visualize import plot_line, plot_value_counts
-
-# Define plotting function to generate plot of gap stats with error bars
+from .visualize import plot_value_counts
 
 
 def silplot(X, cluster_labels, clusterer, pointlabels=None, height=6):
     """Generates silhouette subplot of kmeans clusters alongside PCA n=2
+
+    Two side-by-side subplots are generated showing (1) the silhouette plot of
+    the clusterer's results and (2) the PCA 2-dimensional reduction of the
+    input data, color-coded by cluster.
 
     Source: The majority of the code from this function was provided as a
             helper function from the CS109b staff in HW2
@@ -79,9 +57,18 @@ def silplot(X, cluster_labels, clusterer, pointlabels=None, height=6):
             The original code authored by the cs109b teaching staff
             is modified from:
             http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html
- 
-    """
 
+    :param X: original mutli-dimensional data values against which you are plotting
+    :type X: pandas.DataFrame
+    :param cluster_labels: list of labels for each observation in ``X``
+    :type cluster_labels: list or array
+    :param clusterer: fitted sklearn kmeans clustering object
+    :type clusterer: sklearn.cluster.KMeans object
+    :param pointlabels: list of labels for each point, defaults to None
+    :type pointlabels: list or None, optional
+    :param height: height of resulting subplots, defaults to 6
+    :type height: int, optional
+    """
     n_clusters = clusterer.n_clusters
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, height))
@@ -202,6 +189,12 @@ def silplot(X, cluster_labels, clusterer, pointlabels=None, height=6):
 
 def display_gapstat_with_errbars(gap_df, height=4):
     """Generates plots of gap stats with error bars for each number of clusters
+
+    :param gap_df: dataframe attribute of a fitted ``gap_statistic.OptimalK`` object
+            for plotting (i.e. ``OptimalK.gap_df``)
+    :type gap_df: DataFrame
+    :param height: hieght of the resulting plot, defaults to 4
+    :type height: int, optional
     """
     gaps = gap_df["gap_value"].values
     diffs = gap_df["diff"]
@@ -232,11 +225,22 @@ def display_gapstat_with_errbars(gap_df, height=4):
     plt.show()
 
 
-# Define functions for identifying epsilon values, fitting dbscan, and evaluating results
+# Define functions for identifying epsilon values, fitting dbscan,
+# and evaluating results
 
 
 def fit_neighbors(data, min_samples):
     """Fits n nearest neighbors based on min samples and returns distances
+
+    This is a simple implementation of the ``sklearn.neighbors.NearestNeighbors``
+    and returns the distance results from that object's ``fitted_neighbors`` method
+
+    :param data: data on which to perform nearest neighbors algorithm
+    :type data: dataframe or array
+    :param min_samples: number of neighbors to use by default for kneighbors queries
+    :type min_samples: int
+    :return: array representing the lengths to points
+    :rtype: array
     """
     fitted_neigbors = NearestNeighbors(n_neighbors=min_samples).fit(data)
     distances, indices = fitted_neigbors.kneighbors(data)
@@ -245,6 +249,15 @@ def fit_neighbors(data, min_samples):
 
 def plot_epsilon(distances, min_samples, height=5):
     """Plot epsilon by index sorted by increasing distance
+
+    Generates a line plot of epsilon with observations sorted by increasing distances
+
+    :param distances: distances generated by :func:`fit_neighbors`
+    :type distances: array
+    :param min_samples: number of neighbors used to generate distances
+    :type min_samples: int
+    :param height: height of plot, defaults to 5
+    :type height: int, optional
     """
     fig, ax = plt.subplots(figsize=(12, height))
 
@@ -256,7 +269,7 @@ def plot_epsilon(distances, min_samples, height=5):
     dist_to_nth_nearest_neighbor = distances[:, -1]
     plt.plot(np.sort(dist_to_nth_nearest_neighbor), color="k")
     plt.xlabel("Index (sorted by increasing distances)", fontsize=16)
-    plt.ylabel("$\epsilon$", fontsize=18)
+    plt.ylabel(r"$\epsilon$", fontsize=18)
     plt.tick_params(right=True, labelright=True, labelsize=14)
     plt.grid(":", alpha=0.4)
     plt.tight_layout()
@@ -264,13 +277,50 @@ def plot_epsilon(distances, min_samples, height=5):
 
 
 def silscore_dbscan(data, labels, clustered_bool):
-    """Generates sil score ommitting observations not assigned to any cluster by dbscan 
+    """Generates sil score ommitting observations not assigned to any cluster by dbscan
+
+    :param data: original data used for dbscan clustering
+    :type data: array or dataframe
+    :param labels: cluster label for each observation in ``data``
+    :type labels: array
+    :param clustered_bool: boolean value for each observation indicated whether it had
+            been clustered by dbscan
+    :type clustered_bool: list or 1-d array
+    :return: silhouette score
+    :rtype: float
     """
     return silhouette_score(data[clustered_bool], labels[clustered_bool])
 
 
 def fit_dbscan(data, min_samples, eps):
     """Fits dbscan and returns dictionary of results including model, labels, indices
+
+    :param data: original data to be fitted using ``sklearn.cluster.DBSCAN``
+    :type data: array-like
+    :param min_samples: The number of samples (or total weight) in a neighborhood for
+            a point to be considered as a core point. This includes the point itself.
+    :type min_samples: int
+    :param eps: The maximum distance between two samples for one to be considered as
+            in the neighborhood of the other. This is not a maximum bound on the
+            distances of points within a cluster. This is the most important DBSCAN
+            parameter to choose appropriately for your data set and distance function.
+    :type eps: int or float
+    :return: dictionary of results and important characteristics of the fitted
+            DBSCAN algorithm (see NOTE below)
+    :rtype: dict
+
+    NOTE: Dictionary returned includes the following items::
+
+       {
+           "model": DBSCAN(eps=eps, min_samples=min_samples).fit(data),
+           "n_clusters": sum([i != -1 for i in set(model.labels_)]),
+           "labels": model.labels_,
+           "core_sample_indices": model.core_sample_indices_,
+           "clustered_bool": [i != -1 for i in labels],
+           "cluster_counts": pd.Series(labels).value_counts(),
+           "sil_score": silscore_dbscan(data, labels, clustered_bool),
+       }
+
     """
     fitted_dbscan = DBSCAN(eps=eps, min_samples=min_samples).fit(data)
     db_labels = fitted_dbscan.labels_
@@ -295,6 +345,11 @@ def fit_dbscan(data, min_samples, eps):
 
 def print_dbscan_results(dbscan_dict):
     """Prints summary results of fitted dbscan_dict
+
+    [extended_summary]
+
+    :param dbscan_dict: [description]
+    :type dbscan_dict: [type]
     """
     eps = dbscan_dict["model"].eps
     min_samples = dbscan_dict["model"].min_samples
@@ -321,8 +376,8 @@ def print_dbscan_results(dbscan_dict):
     # plot distribution of labels
     plot_value_counts(dbscan_dict["cluster_counts"], "DBSCAN cluster")
     plt.title(
-        "DBSCAN clustering results, min samples$={}$ and $\epsilon={}$"
-        "".format(min_samples, eps),
+        r"DBSCAN clustering results, min samples$={}$ and $\epsilon={}$"
+        r"".format(min_samples, eps),
         fontsize=16,
     )
     plt.xlabel("DBSCAN clusters (-1 indicates unclustered)", fontsize=12)
@@ -348,7 +403,7 @@ def plot_dendrogram(
     linkage_data, method_name, yticks=16, ytick_interval=1, height=4.5
 ):
     """Plots a dendrogram given a set of input hierarchy linkage data
-    
+
     :param linkage_data: np.array output from scipy.cluster.hierarchy, which
                          should have been applied to a distance matrix to
                          convert it to linkage data
@@ -359,7 +414,7 @@ def plot_dendrogram(
     :param ytick_interval: integer, the desired interval for the resulting
                            y ticks
     :param height: float, the desired height of the resulting plot
-    
+
     return: plots dendrogram, no objects are returned
     """
 
@@ -381,8 +436,7 @@ def plot_dendrogram(
 
 
 # from dataclasses import dataclass
-import hdbscan
-import umap
+
 
 # from pickle import dump, load
 # import plotly.io as pio
@@ -476,7 +530,6 @@ class UMAP_embedder:
             1. columns needed to be added to harmonize with entire data
             2. dummified df before adding columns of [1]
         """
-        raw_df = df[self.initial_columns]
         df_to_transform = df[self.scale_cols]  # .drop(columns=["PID"])
         transformed_columns = pd.DataFrame(
             self.scaler.transform(df_to_transform),
@@ -571,7 +624,7 @@ class UMAP_embedder:
     def get_clustering(self, attributes_2D_mapping):
         assert attributes_2D_mapping.shape[1] == 2
         new_labels = hdbscan.approximate_predict(
-            clusterer, attributes_2D_mapping
+            self.clusterer, attributes_2D_mapping
         )
         return new_labels
 
@@ -581,7 +634,8 @@ def make_spider(mean_peaks_per_cluster, row, name, color):
     categories = list(mean_peaks_per_cluster)[1:]
     N = len(categories)
 
-    # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
+    # What will be the angle of each axis in the plot?
+    # (we divide the plot / number of variable)
     angles = [n / float(N) * 2 * pi for n in range(N)]
     angles += angles[:1]
 
@@ -600,8 +654,6 @@ def make_spider(mean_peaks_per_cluster, row, name, color):
     # plt.yticks([10,20,30], ["10","20","30"], color="grey", size=7)
     # plt.ylim(0,40)
 
-    # Ind1
-    scaled = mean_peaks_per_cluster.loc[row].drop("group").values
     values = (
         mean_peaks_per_cluster.loc[row].drop("group").values.flatten().tolist()
     )
@@ -694,7 +746,7 @@ def plot_umap_scatter(
     )
     plt.xlabel(xlabel, fontsize=14)
     plt.ylabel(ylabel, fontsize=14)
-    cbar = plt.colorbar(scatter, label=scale_var.replace("_", " "))
+    plt.colorbar(scatter, label=scale_var.replace("_", " "))
     ax.grid(":", alpha=0.5)
     plt.tight_layout()
     plt.show()
